3e4c463a0fae566f1c0a5da6560c7faec9e867e0
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-05 08:29:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,18 @@ end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
-
+def turn(board)
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_token = current_player(board)
+    move(board, index, player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 def turn_count(board)
   turn = 0
   board.each do |index|
@@ -53,18 +64,7 @@ def current_player(board)
   return player
 end
 
-def turn(board)
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    player_token = current_player(board)
-    move(board, index, player_token)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
